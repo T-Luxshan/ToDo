e2537abb05dcc2c11c74201faefca58b862daa7f
T-Luxshan <luckybraveboys@gmail.com>
2024-11-17 14:30:09 +0530

    deleteTask -> deleteTaskById

--- src/Task.jsx
+++ src/Task.jsx
@@ -13,7 +13,7 @@ export const Task = (props) => {
                   <div className="col-4 listBtns">
                       <button className="btn btn-outline-success listBtn1" onClick={() => props.taskCompleted(props.id)}>complete</button>
                       &nbsp;&nbsp;
-                      <button className="btn btn-outline-danger listBtn2"  onClick={() => props.deleteTask(props.id)}>X</button>
+                      <button className="btn btn-outline-danger listBtn2"  onClick={() => props.deleteTaskById(props.id)}>X</button>
                   </div>
                 </div>
             </div>
