d8a83d21d0b83fec280d0c00f26ae19698d51154
T-Luxshan <luckybraveboys@gmail.com>
2024-11-17 12:50:13 +0530

    fetch all task API

--- src/service/ToDoService.js
+++ src/service/ToDoService.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = "http://localhost:8080/api/v1";
+const REST_API_BASE_URL = "http://localhost:8080/api/v1/tasks";
 
 export const addNewTask = (task, completed) => {
-    return axios.post(`${REST_API_BASE_URL}/addTask`, {
+    return axios.post(`${REST_API_BASE_URL}`, {
       task, completed
     });
   };
+
+export const fetchAllTask = () => {
+  return axios.get(`${REST_API_BASE_URL}`)
+}
+
+export const deleteTask = (id) => {
+  return axios.delete(`${REST_API_BASE_URL}`, {id})
+}
   
